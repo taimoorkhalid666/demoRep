22572547a7ca641daa040776dec0b3b6633710f7
Taimoor Khalid <taimoor.khalid.jutt@hotmail.com>
2019-11-18 11:34:28 +0000

    first commit

---
+++ SimpledotNetApp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace SimpledotNetApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             Trace.Listeners.Add(new ConsoleTraceListener());
+            for (int i = 0; i < 10; i++)
+                Trace.TraceInformation("App is working ");
+
+            Console.ReadKey();
+        }
+    }
+}
